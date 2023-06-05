b05cd3666e3c707d5cf2c7dea8b9c121533e954e
Sebastian Meza <sebasmezab@gmail.com>
2023-06-05 16:45:36 -0500

    env

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5UtClQKwJjmJKDj-6JuZ3cktTK8Qv-mM",
+  apiKey: import.meta.env.DB_PASSWORD,
   authDomain: "proyecto-react-b7001.firebaseapp.com",
   projectId: "proyecto-react-b7001",
   storageBucket: "proyecto-react-b7001.appspot.com",
