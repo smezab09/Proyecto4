60709ebf3c78b5b9f722c929f824d5c64d47a47a
Sebastian Meza <sebasmezab@gmail.com>
2023-06-07 23:03:08 -0500

    responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,45 +1,53 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black">
-      <Link to="/" className="navbar-brand mx-3" style={{ color: 'lightcoral' }}>
-        Gosal&apos;s Pizza
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
-              Inicio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/reserva" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
-              Reserva
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/menu" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
-              Menú
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contacto" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
-              Contacto
-            </Link>
-          </li>
-        </ul>
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand mx-3" style={{ color: 'lightcoral' }}>
+          Gosal&apos;s Pizza
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleMenu}
+          aria-controls="navbarNav"
+          aria-expanded={isMenuOpen ? 'true' : 'false'}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse${isMenuOpen ? ' show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/reserva" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
+                Reserva
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/menu" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
+                Menú
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contacto" className="nav-link mx-3" style={{ color: 'lightcoral' }}>
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 const Home = () => {
+
   const containerStyle = {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
@@ -21,47 +22,67 @@ const Home = () => {
     height: '100%',
   };
 
+  const containerStyleMobile = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2rem',
+    marginBottom: '2rem',
+  };
+
+  const imageStyleMobile = {
+    width: '100%',
+    height: 'auto',
+    borderRadius: '5px',
+    display: 'flex',
+  };
+
+  const textContainerStyleMobile = {
+    textAlign: 'justify',
+    height: '100%',
+  };
+
+  const isMobile = window.innerWidth <= 768; 
   return (
     <>
-    <div className="container" style={{ padding: '2rem' }}>
-      <header>
-      <h1 style={{ color: 'red', fontSize: '2rem', marginBottom: '1rem' }}>¡Bienvenido a Gosal&apos;s Pizza!</h1>
-      </header>
-      <div style={containerStyle}>
-        <div style={textContainerStyle}>
-          <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
-            ¡Descubre el sabor auténtico de nuestras deliciosas pizzas hechas con ingredientes frescos y de la mejor calidad!
-            Nuestro menú cuenta con una variedad de sabores irresistibles que te harán volver por más.
-            ¡Ven y disfruta de una experiencia culinaria única en Gosals Pizza!
-          </p>
-        </div>
-        <div style={{ textAlign: 'right' }}>
-          <img src="src\images\gosals1.png" alt="Gosals1" style={imageStyle} />
-        </div>
-        <div style={{ textAlign: 'right' }}>
-          <img src="src\images\gosals2.png" alt="Gosals2" style={imageStyle} />
-        </div>
-        <div style={textContainerStyle}>
-          <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
-            ¿Qué esperas para probar nuestras pizzas recién salidas del horno y disfrutar de cada bocado lleno de sabor?
-            Contamos con opciones para todos los gustos, desde clásicas margaritas hasta innovadoras pizzas gourmet.
-            Ven a Gosals Pizza y déjate sorprender por nuestros exquisitos sabores y atención personalizada.
-          </p>
-        </div>
-        <div style={textContainerStyle}>
-          <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
-            ¡Promoción especial de junio para los amantes de la pizza!
-          </p>
-          <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
-            Esta promoción es para ustedes. Por la compra de una pizza mediana familiar, te llevas los bordes de queso gratis.
-            ¡Aprovecha esta deliciosa oferta por tiempo limitado!
-          </p>
-        </div>
-        <div style={{ textAlign: 'right' }}>
-          <img src="src\images\gosals3.png" alt="Pizza 3" style={imageStyle} />
+      <div className="container" style={{ padding: '2rem' }}>
+        <header>
+          <h1 style={{ color: 'red', fontSize: '2rem', marginBottom: '1rem' }}>¡Bienvenido a Gosal&apos;s Pizza!</h1>
+        </header>
+        <div style={isMobile ? containerStyleMobile : containerStyle}>
+          <div style={isMobile ? textContainerStyleMobile : textContainerStyle}>
+            <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
+              ¡Descubre el sabor auténtico de nuestras deliciosas pizzas hechas con ingredientes frescos y de la mejor calidad!
+              Nuestro menú cuenta con una variedad de sabores irresistibles que te harán volver por más.
+              ¡Ven y disfruta de una experiencia culinaria única en Gosals Pizza!
+            </p>
+          </div>
+          <div style={{ textAlign: 'right' }}>
+            <img src="src\images\gosals1.png" alt="Gosals1" style={isMobile ? imageStyleMobile : imageStyle} />
+          </div>
+          <div style={{ textAlign: 'right' }}>
+            <img src="src\images\gosals2.png" alt="Gosals2" style={isMobile ? imageStyleMobile : imageStyle} />
+          </div>
+          <div style={isMobile ? textContainerStyleMobile : textContainerStyle}>
+            <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
+              ¿Qué esperas para probar nuestras pizzas recién salidas del horno y disfrutar de cada bocado lleno de sabor?
+              Contamos con opciones para todos los gustos, desde clásicas margaritas hasta innovadoras pizzas gourmet.
+              Ven a Gosals Pizza y déjate sorprender por nuestros exquisitos sabores y atención personalizada.
+            </p>
+          </div>
+          <div style={isMobile ? textContainerStyleMobile : textContainerStyle}>
+            <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
+              ¡Promoción especial de junio para los amantes de la pizza!
+            </p>
+            <p style={{ color: 'black', fontSize: '1.2rem', marginBottom: '0.5rem' }}>
+              Esta promoción es para ustedes. Por la compra de una pizza mediana familiar, te llevas los bordes de queso gratis.
+              ¡Aprovecha esta deliciosa oferta por tiempo limitado!
+            </p>
+          </div>
+          <div style={{ textAlign: 'right' }}>
+            <img src="src\images\gosals3.png" alt="Pizza 3" style={isMobile ? imageStyleMobile : imageStyle} />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
